2624c016776facb77d22463a13c31eae95b62677
yburatti <yoann.buratti@gmail.com>
2020-02-04 16:25:41 +1100

    Created TDD folder

---
+++ TDD/TDD.py
@@ -0,0 +1,24 @@
+# %%--  Playground
+%reload_ext autoreload
+%autoreload 2
+# %%-
+
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+#---    Initialization
+#///////////////////////////////////////////
+# %%--  Imports
+import DPML
+from DPML import *
+import DPML as d
+# %%-
+
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+#---    Test function
+#///////////////////////////////////////////
+# %%--  Class functionalities and definition
+defect.defect.random_db(100,Et_min = -0.55, Et_max = 0.55, S_min = 1E-17, S_max = 1E-13, Nt = None)
+dir()
+pyDPML.__builtins__
+
+d.defect.random_db
+# %%-
