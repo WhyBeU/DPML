3a6de73e252239192aab6647b3a9f182f91de750
yburatti <yoann.buratti@gmail.com>
2020-02-04 16:38:21 +1100

    added author and description

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from distutils.core import setup
 setup(
     name='DPML',
     version='0.1dev',
+    author='Yoann Buratti',
     packages=['DPML',],
     license='  GNU GENERAL PUBLIC LICENSE',
+    description="Machine learning assisted extraction of defect parameters",
     long_description=open('README.txt').read(),
 )
