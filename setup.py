17760af185aec356ffaaacd324fb0043e67db2b1
yburatti <yoann.buratti@gmail.com>
2020-02-04 16:25:50 +1100

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='DPML',
+    version='0.1dev',
+    packages=['DPML',],
+    license='  GNU GENERAL PUBLIC LICENSE',
+    long_description=open('README.txt').read(),
+)
