e5cb0778d021f4280a26e2d4a268c7e40b64831e
yburatti <yoann.buratti@gmail.com>
2020-02-26 12:39:07 +1100

    updated pycache and __init__ files

--- DPML/main/__init__.py
+++ DPML/main/__init__.py
@@ -1,9 +1,11 @@
 # from .dpss import DPSS
 from .experiment import Experiment
 # from .ml import ML
+# from .simulation import Simulation
 
 __all__ = [
     # 'DPSS',
     'Experiment',
     # 'ML',
+    # 'Simulation',
 ]
