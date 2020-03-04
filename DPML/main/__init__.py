a15ba887c8181b2e0dc67cde2940f2359e82ed85
yburatti <yoann.buratti@gmail.com>
2020-03-04 11:39:33 +1100

    pycache update

--- DPML/main/__init__.py
+++ DPML/main/__init__.py
@@ -1,11 +1,11 @@
 # from .dpss import DPSS
+from .ml import ML
 from .experiment import Experiment
-# from .ml import ML
 # from .simulation import Simulation
 
 __all__ = [
     # 'DPSS',
     'Experiment',
-    # 'ML',
+    'ML',
     # 'Simulation',
 ]
