12b310b0d5cd88500446c7048c2663e4d9fa45a3
yburatti <yoann.buratti@gmail.com>
2020-03-06 16:42:01 +1100

    added plotting. Started logging

--- DPML/main/ml.py
+++ DPML/main/ml.py
@@ -1,6 +1,7 @@
 "Machine learning functions associated to SRH parameter extraction"
 from ..utils.matplotlibstyle import *
 from ..utils import SaveObj, LoadObj
+from ..utils import Logger
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -20,19 +21,52 @@ from sklearn.model_selection import *
 
 class ML():
     #****   Constant declaration    ****#
-    DefaultParameters = {}
+    DefaultParameters = {
+        'name':'',
+        'save': False,
+        'logML': False,
+    }
 
-    #****   Method declaration      ****#
-    def __init__(self,dataset):
-        #   Check applicability of method
+    #****   Core methods      ****#
+    def __init__(self, Dataset, SaveDir, Parameters=None):
+
+        #   Use default if not defined
+        self.parameters=ML.DefaultParameters
+        if Parameters is not None: self.updateParameters(Parameters)
 
         #   Create directory for computation on this dataset
-
+        self.pathDic = {
+            'savedir':      SaveDir,
+            'figures':      SaveDir+"\\figures\\",
+            'objects':      SaveDir+"\\objects\\",
+            'traces':       SaveDir+"\\traces\\",
+            'outputs':      SaveDir+"\\outputs\\",
+        }
+        for key, value in self.pathDic.items():
+                if key in ['figures','objects','traces','outputs']:
+                    if not os.path.exists(value):   os.makedirs(value)
         #   define hyper parameters for ML training
-        self.dataset = dataset
-        self.logID = None
+        self.dataset = Dataset.copy(deep=True)
         self.logTrain={}
+        self.pathDic['logfile']=self.pathDic['traces']+self.parameters['name']++"_"+datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")+".txt"
+        if self.parameters['logML']: self.logger = Logger(self.pathDic['logfile'])
 
+        #   Print header for logfile
+        if self.parameters['logML']: self.logger.open()
+        print(">"*(Logger.TitleLength*2))
+        print(" "*np.max([0,np.int(((Logger.TitleLength*2)-len(self.parameters['name']))/2)])+self.parameters['name'])
+        print("<"*(Logger.TitleLength*2))
+        print("\n")
+        Logger.printTitle('HYPER-PARAMETERS')
+        Logger.printDic(self.parameters)
+        Logger.printTitle('PATH')
+        Logger.printDic(self.pathDic)
+        if self.parameters['logML']: self.logger.close()
+    def updateParameters(self,Parameters):
+        for key,value in Parameters.items():
+            self.parameters[key]=value
+
+    #****   ML methods      ****#
     def trainRegressor(self, targetCol, trainParameters=None):
         #   define and update training parameters:
         trainParam={
@@ -51,19 +85,23 @@ class ML():
             'target_col':targetCol,
             'train_parameters':trainParam,
         }
+        dfAll =  self.dataset.copy(deep=True)
         #   Normalize dataset
         if trainParam['normalize']:
             scaler_dict = {}
             for col in self.dataset.columns:
                 if col in ['Name','bandgap']: continue
                 scaler_dict[col]=MinMaxScaler()
-                self.dataset[col]=scaler_dict[col].fit_transform(self.dataset[col].values.reshape(-1,1))
+                if col not in ["Name","Et_eV","Sn_cm2","Sp_cm2",'k','logSn','logSp','logk','bandgap']: dfAll[col]=np.log10(dfAll[col])
+                dfAll[col]=scaler_dict[col].fit_transform(dfAll[col].values.reshape(-1,1))
         else:
             scaler_dict=None
 
         self.logTrain['scaler']=scaler_dict
         #   Prepare Dataset for training
-        dfTrain, dfVal = train_test_split(self.dataset, test_size=trainParam['validation_fraction'],random_state=trainParam['random_seed'])
+        if  trainParam['bandgap'] == 'upper': dfAll = dfAll.loc[dfAll['bandgap']==1]
+        if  trainParam['bandgap'] == 'lower': dfAll = dfAll.loc[dfAll['bandgap']==0]
+        dfTrain, dfVal = train_test_split(dfAll, test_size=trainParam['validation_fraction'],random_state=trainParam['random_seed'])
         xTrain = dfTrain.drop(["Name","Et_eV","Sn_cm2","Sp_cm2",'k','logSn','logSp','logk','bandgap'],axis =1)
         yTrain = dfTrain[targetCol]
         xVal = dfVal.drop(["Name","Et_eV","Sn_cm2","Sp_cm2",'k','logSn','logSp','logk','bandgap'],axis =1)
@@ -178,3 +216,34 @@ class ML():
         dfVal_output['predicted'] = predVal
         dfVal_output['predicted_proba'] = [np.max(proba) for proba in probaVal]
         self.logTrain[trainKey]['validation_data']= dfVal_output
+
+    #****   Plotting methods      ****#
+    def plotRegressor(self,trainKey, plotParameters=None):
+        plotParam={
+            'figsize':(8,8),
+            'xlabel':'True value',
+            'ylabel':'Predicted value',
+            'legend':True,
+            'show_yx':True,
+            'log_plot':False,
+            'scatter_alpha':0.8,
+            'scatter_s':15,
+            'scatter_c': 'C8'
+        }
+
+        if plotParameters!=None:
+            for key in plotParameters.keys(): plotParam[key]=plotParameters[key]
+
+        targetCol, bandgapParam = trainKey.rsplit('_',1)
+        df_Val = self.logTrain[trainKey]['validation_data']
+        results = self.logTrain[trainKey]['results']
+        plt.figure(figsize=plotParam['figsize'])
+        ax = plt.gca()
+        ax.set_xlabel(plotParam['xlabel'])
+        ax.set_ylabel(plotParam['ylabel'])
+        ax.set_aspect('equal')
+
+        ax.annotate("$\it{R^2}=$%s"%(results['validation_r2']),xy=(0.05,0.95),xycoords='axes fraction', fontsize=14)
+        ax.set_title(targetCol+ " on "+bandgapParam+" bandgap", fontsize=14)
+        ax.scatter(df_Val.actual,df_Val.predicted,marker=".",alpha=plotParam['scatter_alpha'],s=plotParam['scatter_s'],c=plotParam['scatter_c'])
+        if plotParam['show_yx']: ax.plot([np.min([df_Val.actual.min(),df_Val.predicted.min()]),np.max([df_Val.actual.max(),df_Val.predicted.max()])],[np.min([df_Val.actual.min(),df_Val.predicted.min()]),np.max([df_Val.actual.max(),df_Val.predicted.max()])],'k--')
