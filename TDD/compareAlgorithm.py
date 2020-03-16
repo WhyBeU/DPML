0c7c4e4623290e56669f412ef7483a2fcc46ae55
yburatti <yoann.buratti@gmail.com>
2020-03-17 10:10:44 +1100

    updated to doping and temperature covariation

--- TDD/compareAlgorithm.py
+++ TDD/compareAlgorithm.py
@@ -18,23 +18,22 @@ import numpy as np
 # %%--  Inputs
 SAVEDIR = "C:\\Users\\z5189526\\OneDrive - UNSW\\Yoann-Projects\\1-ML-based TIDLS Solver\\06-Github-TDD-savedire\\compareAlgorithm\\"
 # TEMPERATURE = [200,250,300,350,400]
-TEMPERATURE = [300,300,300,300,300]
+TEMPERATURE = [200,200,200,200,200,250,250,250,250,250,300,300,300,300,300,350,350,350,350,350,400,400,400,400,400]
+# TEMPERATURE = [300,300,300,300,300]
 # DOPING = [1e15,1e15,1e15,1e15,1e15]
-DOPING = [5e14,1e15,5e15,1e16,5e16]
-WAFERTYPE = 'p'
+# DOPING = [5e14,1e15,5e15,1e16,5e16]
+DOPING = [5e14,1e15,5e15,1e16,5e16,5e14,1e15,5e15,1e16,5e16,5e14,1e15,5e15,1e16,5e16,5e14,1e15,5e15,1e16,5e16,5e14,1e15,5e15,1e16,5e16]
+WAFERTYPE = 'n'
 # %%-
 # %%--  Hyper-parameters
 PARAMETERS = {
-    'name': 'test',
-    'save': False,   # True to save a copy of the printed log, the outputed model and data
+    'name': 'Doping-temperature-combine-variation-ntype',
+    'save': True,   # True to save a copy of the printed log, the outputed model and data
     'logML':True,   #   Log the output of the console to a text file
-    'n_defects': 1000, # Size of simulated defect data set for machine learning
-<<<<<<< Updated upstream
-    'dn_range' : np.logspace(13,17,10),# Number of points to interpolate the curves on
-=======
+    'n_defects': 100000, # Size of simulated defect data set for machine learning
     'dn_range' : np.logspace(13,17,100),# Number of points to interpolate the curves on
->>>>>>> Stashed changes
-    'training_keys': ['Et_eV_upper','Et_eV_lower','logk_upper','logk_lower'] # for k prediction
+    'regression_training_keys': ['Et_eV_upper','Et_eV_lower','logk_all'], # for k prediction
+    'classification_training_keys': ['bandgap_all'], # for k prediction
     # 'training_keys': ['Et_eV_upper','Et_eV_lower','logSn_upper','logSn_lower','logSp_upper','logSp_lower'] # for Sn,Sp prediction
 }
 # %%-
@@ -46,48 +45,27 @@ PARAMETERS = {
 exp = Experiment(SaveDir=SAVEDIR, Parameters=PARAMETERS)
 exp.updateParameters({'type':WAFERTYPE,'temperature':TEMPERATURE,'doping':DOPING})
 exp.generateDB()
-exp.saveExp()
+# exp.saveExp()
 # %%-
 
 # %%--  Train machine learning algorithms
-exp = Experiment.loadExp(SAVEDIR+"objects\\", filename='experimentObj_Doping variation_2020-03-06-11-33-49')
+# exp = Experiment.loadExp(SAVEDIR+"objects\\", filename='experimentObj_Doping variation_2020-03-06-11-33-49')
 ml = exp.newML()
-for trainKey in exp.parameters['training_keys']:
+for trainKey in exp.parameters['regression_training_keys']:
     targetCol, bandgapParam = trainKey.rsplit('_',1)
     ml.trainRegressor(targetCol=targetCol, trainParameters={'bandgap':bandgapParam})
-    ml.plotRegressor(trainKey, plotParameters={'scatter_c':'darkred'})
+for trainKey in exp.parameters['classification_training_keys']:
+    targetCol, bandgapParam = trainKey.rsplit('_',1)
+    ml.trainClassifier(targetCol=targetCol, trainParameters={'bandgap':bandgapParam})
+for trainKey in exp.parameters['regression_training_keys']: ml.plotRegressor(trainKey, plotParameters={'scatter_c':'black'})
+#
 
-ml.trainRegressor(targetCol='Et_eV', trainParameters={'bandgap':'upper'})
-len(ml.dataset)
-
-mlID=exp.updateLogMLmodel(ml,ml.logID)
+# exp.saveExp()
+# %%-
+# %%--  Save
+ml.saveML()
 exp.saveExp()
 # %%-
-# %%--  Make ML predictions
-ML.DefaultParameters
-# %%-
-df_Val = ml.logTrain['Et_eV_upper']['validation_data']
-
-# %%--
-exp = Experiment(SaveDir=SAVEDIR, Parameters=PARAMETERS)
-exp.loadCSV(FilePath=FILEPATH,Temperature=TEMPERATURE,Doping=DOPING, Type=WAFERTYPE)
-exp.interpolateSRH()
-exp.plotSRH()
-exp.generateDB()
-exp.saveExp()
-exp.loadLTS()
-exp.logbook
-exp.logDataset['0']
-
-exp = Experiment.loadExp(SAVEDIR+"objects\\")
-ml = exp.newML()
-ml.__dict__
-ml.logTrain
-ml.trainRegressor(targetCol='Et_eV')
-ml.trainClassifier(targetCol='bandgap')
-<<<<<<< Updated upstream
-=======
-
 
 # %%--  Inspect
 exp.logbook
@@ -99,4 +77,3 @@ exp.__dict__
 ml.logger = None
 np.power(10,exp.predictCsv['0']['logk_all'])
 # %%-
->>>>>>> Stashed changes
