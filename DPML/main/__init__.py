5344b9e2dd3a9a704ff9917e01f434594200aaad
yburatti <yoann.buratti@gmail.com>
2020-07-23 09:40:20 +1000

    clean-up phase

--- DPML/main/__init__.py
+++ DPML/main/__init__.py
@@ -1,11 +1,7 @@
-# from .dpss import DPSS
 from .ml import ML
 from .experiment import Experiment
-# from .simulation import Simulation
 
 __all__ = [
-    # 'DPSS',
     'Experiment',
     'ML',
-    # 'Simulation',
 ]
