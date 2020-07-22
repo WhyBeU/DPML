4b918806cf4a3c0ae9ab303c2460e29280d4861f
yburatti <yoann.buratti@gmail.com>
2020-07-23 08:22:56 +1000

    Comments and instructions

--- example/ML-dataset-size-analysis.py
+++ example/ML-dataset-size-analysis.py
@@ -4,15 +4,38 @@
 # %%--  Imports
 from DPML import *
 import numpy as np
+from DPML.utils.matplotlibstyle import *
+import matplotlib.pyplot as plt
+# %%-
+
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+#---    Setup
+#///////////////////////////////////////////
+# %%--  Instructions:
+'''
+---Main Steps---
+      Choose SAVEDIR folder where to save the output files from DPML. (Use absolute path)
+      Provide TEMPERATURE as a list of the temperature in Kelvin for each measurements
+      Provide DOPING as a list of the temperature in cm-3 for each measurements
+      Provide cell type 'n' or 'p'
+      NAME your experiment
+
+---Other notes---
+    Change hyper-parameters as desired.
+    Adjust file specific inputs
+    Adjust regression_training_keys to desired value: Et_upper, Et_lower or logk_all
+'''
 # %%-
 
 # %%--  Inputs
 SAVEDIR = "C:\\Users\\z5189526\\Documents\\GitHub\\DPML\\savedir_example\\"
-# FILEPATH = "C:\\Users\\z5189526\\Documents\\GitHub\\DPML\\example\\sample.csv"
 TEMPERATURE = [200,250,300,350,400]
 DOPING = [1e15,1e15,1e15,1e15,1e15]
 WAFERTYPE = 'p'
 NAME = 'Dataset size dependecy'
+#   File specific inputs
+RANGE = np.logspace(2,5,100)
+
 # %%-
 
 # %%--  Hyper-parameters
@@ -22,13 +45,13 @@ PARAMETERS = {
     'logML':False,   #   Log the output of the console to a text file
     'n_defects': 100000, # Size of simulated defect data set for machine learning
     'dn_range' : np.logspace(13,17,100),# Number of points to interpolate the curves on
-    'classification_training_keys': ['bandgap_all'], # for k prediction
-    'regression_training_keys': ['logk_all'], # for k prediction
+    'classification_training_keys': ['bandgap_all'], # for parameter prediction
+    'regression_training_keys': ['logk_all'], # for parameter prediction
 }
 # %%-
 
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-#---    Dependencies to dataset size
+#---    Script
 #///////////////////////////////////////////
 
 # %%--  Define experiment and generate defect database
@@ -38,7 +61,6 @@ exp.generateDB()
 # %%-
 
 # %%--  Loop
-RANGE = np.logspace(2,5,100)
 DB = exp.logDataset['0'].copy(deep=True)
 TIME=[]
 SCORE=[]
@@ -55,8 +77,6 @@ exp.logDataset['0'] = DB.copy(deep=True)
 # %%-
 
 # %%--  Plot
-from DPML.utils.matplotlibstyle import *
-import matplotlib.pyplot as plt
 plotParam={
     'figsize':(6,6),
     'xlabel':'Dataset size',
@@ -87,8 +107,6 @@ ax.semilogx()
 ax.set_title("Random Forest k prediction", fontsize=14)
 ax.scatter(RANGE,SCORE)
 plt.show()
-
-
 # %%-
 
 # %%--  Train machine learning algorithms
